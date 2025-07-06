b73293cc0fdf526bc729c66dab9bbcf52800f062
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-06 14:54:14 +0530

    Test JSON MIME type

--- src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/StudentController.java
+++ src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/StudentController.java
@@ -1,7 +1,11 @@
 package lk.ijse.cmjd_111.CourseRegistration2025.controller;
 
+import lk.ijse.cmjd_111.CourseRegistration2025.dto.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/student")
 public class StudentController {
     @PostMapping
-    public String saveStudent(){
-        return "Student Registration Successfully!";
+    public ResponseEntity<UserDTO> saveStudent(@RequestBody UserDTO userDTO){
+        return new ResponseEntity<>(userDTO,HttpStatus.CREATED);
     }
 }
