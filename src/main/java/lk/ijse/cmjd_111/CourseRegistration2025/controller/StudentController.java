5b043aec7969191a06429da5fd1838987c80753f
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-13 10:52:16 +0530

    Test Get All Students method

--- src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/StudentController.java
+++ src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/StudentController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/student")
 public class StudentController {
@@ -28,5 +31,41 @@ public class StudentController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(student01,HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllStudents(){
+        List<UserDTO> studentList = Arrays.asList(
+                new UserDTO("STU02", "Nimal",
+                        "Fernando", "nimalf@mail.com",
+                        "Lake Road", "Colombo 7",
+                        "Colombo", "Colombo",
+                        "abc123", Role.STUDENT),
+
+                new UserDTO("STU03", "Sithara",
+                        "Jayasinghe", "sithara.j@mail.com",
+                        "Temple Lane", "Galle Fort",
+                        "Galle", "Galle",
+                        "pass456", Role.STUDENT),
+
+                new UserDTO("STU04", "Ruwan",
+                        "Silva", "ruwan.s@mail.com",
+                        "Hill Street", "Kandy Central",
+                        "Kandy", "Kandy",
+                        "kandy321", Role.STUDENT),
+
+                new UserDTO("STU05", "Dilani",
+                        "Hettiarachchi", "dilani.h@mail.com",
+                        "Marine Drive", "Negombo North",
+                        "Negombo", "Negombo",
+                        "negombo789", Role.STUDENT),
+
+                new UserDTO("STU06", "Suresh",
+                        "Wickramasinghe", "suresh.w@mail.com",
+                        "Green Avenue", "Matara West",
+                        "Matara", "Matara",
+                        "matara123", Role.STUDENT)
+        );
+
+        return new ResponseEntity<>(studentList, HttpStatus.OK);
     }
 }
