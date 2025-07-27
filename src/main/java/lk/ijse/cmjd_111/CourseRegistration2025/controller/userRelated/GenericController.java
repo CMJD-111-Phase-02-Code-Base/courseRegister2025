298f83781dfaa4912903bf421f6fc35cb2b39d12
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-27 13:39:13 +0530

    Add Model Mapper to the project

--- src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/userRelated/GenericController.java
+++ src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/controller/userRelated/GenericController.java
@@ -1,11 +1,7 @@
 package lk.ijse.cmjd_111.CourseRegistration2025.controller.userRelated;
 
-import lk.ijse.cmjd_111.CourseRegistration2025.dto.UserDTO;
 import lk.ijse.cmjd_111.CourseRegistration2025.service.GenericService;
-import lk.ijse.cmjd_111.CourseRegistration2025.service.StudentService;
-import lk.ijse.cmjd_111.CourseRegistration2025.service.impl.StudentServiceIMPL;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,43 +18,47 @@ public abstract class GenericController<T> {
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveUser(@RequestBody T user){
-            userService.saveUser(user);
-            return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<Void> saveUser(@RequestBody T user) {
+        userService.saveUser(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
-    @GetMapping(value = "{userId}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<T> getSelectedUser(@PathVariable String userId){
-        if(userId == null){
+
+    @GetMapping(value = "{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<T> getSelectedUser(@PathVariable String userId) {
+        if (userId == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         try {
-              return new ResponseEntity<>(userService.getSelectedUser(userId),HttpStatus.OK);
-        }catch (Exception ex){
+            return new ResponseEntity<>(userService.getSelectedUser(userId), HttpStatus.OK);
+        } catch (Exception ex) {
             ex.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<T>> getAllUsers(){
-         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    public ResponseEntity<List<T>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
+
     @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> updateUser(@RequestParam String userId,
-                              @RequestBody T toBeUpdatedData){
-         try {
-             userService.updateUser(userId,toBeUpdatedData);
-             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-         }catch (Exception ex){
-             ex.printStackTrace();
-             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-         }
+                                           @RequestBody T toBeUpdatedData) {
+        try {
+            userService.updateUser(userId, toBeUpdatedData);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
+
     @DeleteMapping(value = "{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String userId){
+    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
         try {
             userService.deleteUser(userId);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
