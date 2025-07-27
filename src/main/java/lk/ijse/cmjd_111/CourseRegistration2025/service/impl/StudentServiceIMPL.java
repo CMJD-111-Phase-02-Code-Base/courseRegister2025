b7f8bc6270e3002efb5d78cf610a3c27f27f1805
ThanuraSilva <thanurasilva1989@gmail.com>
2025-07-27 14:41:00 +0530

    Minor Change

--- src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/service/impl/StudentServiceIMPL.java
+++ src/main/java/lk/ijse/cmjd_111/CourseRegistration2025/service/impl/StudentServiceIMPL.java
@@ -1,14 +1,24 @@
 package lk.ijse.cmjd_111.CourseRegistration2025.service.impl;
+import lk.ijse.cmjd_111.CourseRegistration2025.dao.StudentDao;
 import lk.ijse.cmjd_111.CourseRegistration2025.dto.UserDTO;
+import lk.ijse.cmjd_111.CourseRegistration2025.entity.StudentEntity;
 import lk.ijse.cmjd_111.CourseRegistration2025.service.GenericService;
+import lk.ijse.cmjd_111.CourseRegistration2025.util.Conversion;
+import lk.ijse.cmjd_111.CourseRegistration2025.util.IDGen;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
+@RequiredArgsConstructor
 public class StudentServiceIMPL implements GenericService<UserDTO> {
+   private final StudentDao studentDao;
+   private final Conversion conversion;
 
     @Override
     public void saveUser(UserDTO user) {
-
+        var studentEntity = conversion.toStudentEntity(user);
+        studentEntity.setId(IDGen.generateStudentID());
+        studentDao.save(studentEntity);
     }
 
     @Override
